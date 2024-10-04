7e9348c51d2af3ba225841865e81ddf8b0b58eb9
msm-faraj <farajzademeysam@gmail.com>
2024-10-04 10:44:29 +0200

    add error handler

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,21 +1,35 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   async function fetchPlaces() {
     setIsFetching(true);
-    const response = await fetch('http://localhost:3000/places');
-    const resData = await response.json();
-    setAvailablePlaces(resData.places);
+
+    try {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      if (!response.ok) {
+        throw new Error('Failed to fetch places');
+      }
+      setAvailablePlaces(resData.places);
+    } catch (error) {
+      setError({ message: error.message || 'My custom error message' });
+    }
     setIsFetching(false);
   }
 
   useEffect(() => {
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message}></Error>;
+  }
 
   return (
     <Places
